8507edb2e5a85560dc5e796898e66a09042343b8
BrunoMatheus10 <brunomatheus88300432@gmail.com>
2024-09-18 23:51:39 -0300

    Add files via upload

---
+++ atividade-tema14-4.py
@@ -0,0 +1,10 @@
+# Função para calcular o pagamento com base nas horas trabalhadas e no valor por hora
+def pagamento(horas_trabalhadas, valor_hora):
+    return horas_trabalhadas * valor_hora
+
+# Leitura das horas trabalhadas e do valor por hora do usuário
+horas = float(input("Digite as horas trabalhadas: "))
+valor = float(input("Digite o valor por hora: "))
+
+# Exibição do valor a ser pago
+print(f"Valor a ser pago: {pagamento(horas, valor)}")
